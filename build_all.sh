45950f63735abc2db5d4e9b5d52396846d165895
Yan Zhai <yanzhai@cs.wisc.edu>
2020-02-25 04:51:55 -0600

    adding new build config for k8s

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,37 @@
 echo "Building K8s"
 cd kubernetes
 # TODO: put k8s build script things here.
+KUBE_FASTBUILD=true build/run.sh make kube-apiserver kube-proxy kube-controller-manager kube-scheduler kubectl kubelet kubeadm
+if [ $? -ne 0 ]; then
+	echo "build fail!"
+	exit 1
+fi
+make quick-release
+# We can do make package later for faster rebuild. But now we need quick-release to get everything done
+#KUBE_FASTBUILD=true make package
+docker image rm kube-apiserver
+tag=`hack/print-workspace-status.sh | grep DOCKER_TAG | awk '{print $2}'`
+# Delete first then reload
+docker image rm k8s.gcr.io/kube-apiserver-amd64:$tag
+docker image rm k8s.gcr.io/kube-apiserver:$tag
+docker image load -i _output/release-images/amd64/kube-apiserver.tar
+bash wcp.sh _output/release-images/amd64/kube-apiserver.tar
+bash wrun.sh "docker image load -i kube-apiserver.tar;"
+
+# edit kubelet config to reload
+sed -i 's/image: .\+/image: k8s.gcr.io\/kube-apiserver-amd64:'$tag'/' /etc/kubernetes/manifests/kube-apiserver.yaml
+
+# wait for api server to reload.
+sleep 5
+
+bash wrun.sh "sudo systemctl stop kubelet;"
+for n in kubelet kubeadm kubectl; do
+  make -j $n
+  bash wcp.sh _output/bin/$n
+  bash wrun.sh "sudo cp $n /usr/bin/"
+done
+bash wrun.sh "sudo systemctl start kubelet;"
+
 )
 
 (
